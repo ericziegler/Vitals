c164490c29e46e0e9058f05d4e26644dce78c848
Eric <eric@zigabytes.com>
2020-05-19 16:24:14 -0400

    EZ: Add icons/assets. Create table view.

--- Vitals/Vitals/MainController.swift
+++ Vitals/Vitals/MainController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class MainController: BaseViewController {
+class MainController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // MARK: - Properties
+
+    @IBOutlet var vitalsTable: UITableView!
 
     // MARK: - Init
 
@@ -19,6 +23,24 @@ class MainController: BaseViewController {
 
     private func setupNavBar() {
         self.title = "Vitals"
+    }
+
+    // MARK: - UITableViewDataSource / UITableViewDelegate
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
     }
 
 }
