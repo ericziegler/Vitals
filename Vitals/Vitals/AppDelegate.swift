403591b75169d2c47b74f4d6b641ab772e09f1da
Eric <eric@zigabytes.com>
2020-05-21 12:13:05 -0400

    EZ: Clean up debug code.

--- Vitals/Vitals/AppDelegate.swift
+++ Vitals/Vitals/AppDelegate.swift
@@ -13,33 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         applyApplicationAppearanceProperties()
-//        var vitals = Vitals()
-//        vitals.date = Date().dateByAddingDays(-1)
-//        vitals.timeOfDay = .am
-//        vitals.weight = 182
-//        vitals.temperature = 96.8
-//        vitals.systolic = 121
-//        vitals.diastolic = 84
-//        vitals.pulse = 78
-//        VitalsLog.shared.add(vitals: vitals)
-//        vitals = Vitals()
-//        vitals.date = Date().dateByAddingDays(-1)
-//        vitals.timeOfDay = .pm
-//        vitals.weight = 184
-//        vitals.temperature = 97.0
-//        vitals.systolic = 130
-//        vitals.diastolic = 93
-//        vitals.pulse = 77
-//        VitalsLog.shared.add(vitals: vitals)
-//        vitals = Vitals()
-//        vitals.date = Date()
-//        vitals.timeOfDay = .am
-//        vitals.weight = 184
-//        vitals.temperature = 97.1
-//        vitals.systolic = 133
-//        vitals.diastolic = 90
-//        vitals.pulse = 85
-//        VitalsLog.shared.add(vitals: vitals)
         return true
     }
 
