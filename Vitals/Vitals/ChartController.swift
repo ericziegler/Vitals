f7f07dce159be8934a5cbc19dbcdb232e45e5d06
Eric <eric@zigabytes.com>
2020-06-08 11:25:02 -0400

    EZ: Setup tabs so we can build a chart view.

---
+++ Vitals/Vitals/ChartController.swift
@@ -0,0 +1,21 @@
+//
+//  ChartController.swift
+//  Vitals
+//
+//  Created by Eric Ziegler on 6/8/20.
+//  Copyright © 2020 Zigabytes. All rights reserved.
+//
+
+import UIKit
+
+class ChartController: BaseViewController {
+
+
+    // MARK: - Init
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Chart"
+    }
+
+}
